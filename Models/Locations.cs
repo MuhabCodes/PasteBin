efdc5f2c75d5e0c24c12e994b89f206278861eb4
Muhab <icecream10800@gmail.com>
2022-02-06 10:25:33 +0200

    updates to pastebin

--- Models/Locations.cs
+++ Models/Locations.cs
@@ -4,9 +4,9 @@ namespace PasteBin.Models
 {
     public class Locations
     {
-        public static string FileLocation = Path.Combine("wwwroot","files");
+        public static string FileLocation = Path.Combine("wwwroot", "files");
         
-        public static string UsersLocation = Path.Combine("wwwroot","users");
+        public static string UsersLocation = Path.Combine("wwwroot", "users");
 
         public static string JsonLocation = Path.Combine("wwwroot", "json");
     }
